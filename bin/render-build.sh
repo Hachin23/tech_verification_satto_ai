c1d304199366b55dec50b7fbfac710b04563d49a
masahito <think.p.hm.0203@gmail.com>
2026-04-09 00:24:16 +0900

    render用のビルドシェルの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-bundle install
-yarn install # または npm install
-bundle exec vite build --mode production
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+# Bundler（本番最適化）
+bundle config set without 'development test'
+bundle install --jobs 4 --retry 3
 
+# Yarn
+yarn install --frozen-lockfile
+# Vite build
+NODE_ENV=production bundle exec vite build
+# Rails assets
+RAILS_ENV=production NODE_ENV=production SKIP_CSS_BUILD=true bundle exec rails assets:precompile
+# DB
+bundle exec rails db:prepare

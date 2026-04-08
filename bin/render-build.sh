b3876dbd6961315dd1c4ed165393a88ab8b6535b
masahito <think.p.hm.0203@gmail.com>
2026-04-09 03:14:12 +0900

    build:cssがなくてもエラーにならないように修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ bundle install
 # Yarn
 yarn install --frozen-lockfile
 # Rails assets（ここでViteも実行される）
-RAILS_ENV=production NODE_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production SKIP_CSS_BUILD=true NODE_ENV=production bundle exec rails assets:precompile
 # DB
 bundle exec rails db:prepare
